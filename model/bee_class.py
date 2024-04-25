86724359ad05b486a25911975acffe5023c4d75f
Deepak Naidu Talapaneni <83692151+DeepakNaiduTalapaneni@users.noreply.github.com>
2024-04-25 16:46:58 -0400

    Update bee_class.py
    Updates the fitness function

--- model/bee_class.py
+++ model/bee_class.py
@@ -17,8 +17,8 @@ class Bee():
     def update_position(self, new_position):
         self.position = new_position
 
-    def update_fitness(self):
-        self.fitness = self.objective_func(self.position)
+     def update_fitness(self):
+        self.fitness = (self.position[0]*0.2+(100-self.position[1])*0.2+self.position[2]*0.2+self.position[3]*0.1+(100-self.position[4])*0.1+self.position[5]*0.1+self.position[6]*0.1)/100
 
     def display(self):
         print(f"I am a Base Bee")
